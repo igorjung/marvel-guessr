f2b89f9150698d3733aa41aac673e455a45fd04d
igorjung <igorc.jung@gmail.com>
2022-05-13 12:47:52 -0300

    Finishing last details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import type { NextPage } from 'next'
-import { useState, useMemo } from 'react'
-import { Help, Info } from '@material-ui/icons'
-import { TextField } from '@material-ui/core';
+import { useState, useMemo, useEffect } from 'react'
+import { Close, Info } from '@material-ui/icons'
+import { TextField, Dialog } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import { 
   startOfDay, 
-  endOfDay, 
   differenceInDays, 
-  differenceInHours,
-  differenceInMinutes,
-  differenceInSeconds,
   format
 } from 'date-fns'
 import { 
@@ -33,7 +29,7 @@ const Container = styled.main`
   max-width: 800px;
   height: 100%;
 
-  padding: 32px;
+  padding: 16px 0;
 `
 const Header = styled.header`
   display: flex;
@@ -46,32 +42,42 @@ const Header = styled.header`
 `
 const Content = styled.div`
   position: relative;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  width: 496px;
-  height: 224px;
-  margin-bottom: 32px;
+  span.answer {
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 12px;
+  }
 `
-const ImageContainer = styled.div`
-  position: relative;
-
+const ImageContainer = styled.div<{guesses: number, isCorrect: boolean}>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  width: 496px;
-  height: 224px;
-  margin-bottom: 32px;
+  height: 360px;
+  width: 800px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border-radius: 8px;
+
+  div {
+    position: relative;
+    width: 360px;
+    height: 360px;
+    filter: ${({ isCorrect, guesses }) => isCorrect ? `blur(0px)` : `blur(${5 - guesses}px)`};
+  }
 `
 const Form = styled.div`
   display: grid;
-  grid-template-columns: 70% 30%;
+  grid-template-columns: 80% 20%;
   width: 100%;
+  padding: 0 64px;
+  margin-top: 32px;
 `
 const Input = styled(TextField)`
   width: 100%;
@@ -88,37 +94,148 @@ const SubmitButton = styled.button`
   color: #fff;
 
   border-radius: 4px;
-  background-color: red;
+  background-color: ${({theme}) => theme.colors.primary};
+  cursor: pointer;
+
+  &:disabled {
+    background-color: ${({theme}) => theme.colors.secondary};
+    cursor: not-allowed;
+  }
 `;
-const Text = styled.p`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+const Text = styled.p<{isCorrect: boolean}>`
+  font-size: 24px;
+  color: ${({isCorrect}) => isCorrect ? '#081B2B' : '#450003'};
+  font-weight: 600;
+  width: 100%;
+  text-align: center;
+  margin-top: 16px;
 `
 const List = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  width: 100%;
+
+  margin: 16px 0 32px 0;
 
   li {
+    width: 100%;
+
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
+
+    text-align: left;
+
+    font-size: 18px;
+    color: ${({ theme }) => theme.colors.text};
+    font-weight: 400;
+
+    svg {
+      color: #450003;
+      font-size: 18px;
+    }
+
+    & + li {
+      margin-top: 8px;
+    }
   }
 `
 const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  margin-top: auto;
+
+  p {
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.text};
+    font-weight: 600;
+
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.colors.primary};
+      font-weight: bold;
+    }
+  }
+`
+
+const Modal = styled(Dialog)`
+`
+const ModalHeader = styled.header`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
 
   width: 100%;
-  height: 60px;
-  margin-top: auto;
+  margin-bottom: 24px;
+  padding: 16px 32px 0 32px;
+
+  h2 {
+    color: ${({ theme }) => theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  button {
+    cursor: pointer;
+
+    svg {
+      color: #000;
+      font-size: 22px;
+    }
+  }
 `
+const ModalSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 0 32px 16px 32px;
+
+  p {
+    font-size: 16px;
+    line-height: 22px;
+    color: ${({ theme }) => theme.colors.text};
+    font-weight: 600;
+    margin-bottom:  16px;
+
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.colors.primary};
+      font-weight: bold;
+    }
+  }
+`
+
+export interface IAbout {
+  open: boolean;
+  onClose: () => void;
+}
+
+const AboutModal = ({open, onClose}: IAbout) => {
+  return (
+    <Modal onClose={onClose} open={open}>
+      <ModalHeader>
+        <h2>ABOUT</h2>
+        <button type="button" onClick={onClose}>
+          <Close />
+        </button>
+      </ModalHeader>
+      <ModalSection>
+        <p>This is a game for Marvel's fans! Inspired by <a href="https://www.nytimes.com/games/wordle/index.html" target="blank">Wordle</a>, <a href="https://term.ooo/" target="blank">Termoo</a>, <a href="https://www.gabtoschi.com/letreco/" target="blank">Letreco</a> and <a href="https://framed.wtf/" target="blank">Framed</a></p>
+        <p>Each day a new character is picked from <a href="https://developer.marvel.com/" target="blank">Marvel's API</a>.</p>
+        <p>To play is quite simple. Guess the character name looking the blur picture. If you get a guess wron the picture becames more clear.</p>
+        <p>All rights go to the rightful owners - no copyright infringement intended.</p>
+      </ModalSection>
+    </Modal>
+  )
+}
 
 interface IHome {
   data: {
@@ -129,20 +246,59 @@ interface IHome {
       extension: string,
     },
   },
+  days: number,
+  options: [{
+    id: number,
+    name: string,
+  }]
 }
 
-const Home: NextPage = ({ data } : IHome) => {
+const Home: NextPage = ({ 
+  data, 
+  days,
+  options
+} : IHome) => {
   const [isCorrect, setIsCorrect] = useState(false);
   const [guesses, setGuesses] = useState([]);
+  const [guess, setGuess] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const setLocalStorage = (key: string, value: string) => {
+    localStorage.setItem(key, value)
+  }
+
+  const getLocalStorage = (key: string) => {
+    return localStorage.getItem(key);
+  }
+
+  const resetGame = () => {
+    const dayNumber = parseInt(getLocalStorage('days') || 0)
+
+    if(days > dayNumber) {
+      setLocalStorage('guesses', JSON.stringify([]))
+      setLocalStorage('isCorrect', 'false')
+    }
+    
+    setLocalStorage('days', `${days}`)
+  }
 
   const handleGuessing = () => {
-    const guess = (document.getElementById('guessing-input') as HTMLInputElement).value
     if(guess === answer) {
       setIsCorrect(true)
+      setLocalStorage(
+        'isCorrect',
+        'true'
+      )
     } else {
       const guessesList = [...guesses, guess]
       setGuesses(guessesList)
+      setLocalStorage(
+        'guesses',
+        JSON.stringify(guessesList)
+      )
     }
+
+    setGuess('')
   }
 
   const [answer, thumbnail] = useMemo(() => {
@@ -156,6 +312,17 @@ const Home: NextPage = ({ data } : IHome) => {
     return ['', '']
   }, [data])
 
+  useEffect(() => {
+    resetGame()
+
+    const guessesList = getLocalStorage('guesses')
+    const status = getLocalStorage('isCorrect')
+
+    setGuesses(JSON.parse(guessesList) || [])
+    setIsCorrect(status === 'true')
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
       <Head>
@@ -167,33 +334,48 @@ const Home: NextPage = ({ data } : IHome) => {
         <Header>
           Marvels
           <div>
-            <Info />
-            <Help />
+            <button type="button" onClick={() => setOpen(true)}>
+              <Info />
+            </button>
           </div>
         </Header>
         <Content>
           {data ? (
             <>
-              <ImageContainer>
-                <Image 
-                  alt='image'
-                  layout='fill'
-                  objectFit='cover'
-                  src={thumbnail}   
-                />
+              <span className="answer">
+                {(isCorrect ||
+                (guesses && guesses.length > 5)) ?
+                  answer :
+                  '???'
+                }
+              </span>
+              <ImageContainer guesses={guesses.length} isCorrect={isCorrect}>
+                <div>
+                  <Image 
+                    alt='image'
+                    layout='fill'
+                    objectFit='cover'
+                    src={thumbnail}   
+                  />
+                </div>
               </ImageContainer>
               {isCorrect ||
               (guesses && guesses.length > 5) ? (
-                <Text>
+                <Text isCorrect={isCorrect}>
                   {isCorrect ? 'You got it!' : 'You missed!'}
                 </Text>
               ) : (
                 <Form>
                   <Autocomplete
-                    freeSolo
                     id="guessing-input"
+                    value={guess || null}
+                    options={options.map((option) => option.name)}
+                    onChange={(event: any, newValue: string | null) => {
+                      setGuess(newValue)
+                    }}
+                    clearOnEscape
                     disableClearable
-                    options={list.characters.map((option) => option.name)}
+                    blurOnSelect
                     renderInput={(params) => (
                       <Input
                         {...params}
@@ -206,15 +388,18 @@ const Home: NextPage = ({ data } : IHome) => {
                     )}
                   />
                   <SubmitButton 
-                    type="button" 
+                    type="button"
+                    disabled={!guess} 
                     onClick={handleGuessing}
-                  >SUBMIT</SubmitButton>
+                  >
+                    SUBMIT
+                  </SubmitButton>
                 </Form>
               )}
               <List>
                 {guesses && guesses.map((item) => (
                   <li key={item}>
-                    <span>X</span>
+                    <Close />
                     {item}
                   </li>
                 ))}
@@ -230,10 +415,12 @@ const Home: NextPage = ({ data } : IHome) => {
           )}
         </Content>
         <Footer>
-          These icons and more are from material-icons
-          These images are from marvel-api
+          <p>
+            This site uses <a href="https://developer.marvel.com/" target="blank">Marvel's API</a> and icons from <a href="https://mui.com/" target="blank">Material UI</a>
+          </p>
         </Footer>
       </Container>
+      <AboutModal open={open} onClose={() => setOpen(false)}/>
     </>
   )
 }
@@ -250,16 +437,24 @@ const getDates = () => {
     format(new Date(), type),
     timeZone
   )
+
   return differenceInDays(date, firstDay)
 }
 
 export const getStaticProps = async () => {
   const days = getDates()
-  const [data] = await getCharacter(list.characters[days].id)
+  const [data] = await getCharacter(list.characters[198].id)
+
+  const options = list.characters
+  options.sort((a, b) => a.id - b.id )
 
   return {
-    revalidate: 300,
-    props: { data }
+    revalidate: 80000,
+    props: { 
+      data, 
+      days,
+      options
+    }
   };
 };
 
