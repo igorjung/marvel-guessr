8ef0e8c3211e55daf93de7da93486a5af258e32b
igorjung <igorc.jung@gmail.com>
2022-05-27 17:38:58 -0300

    Fixing timeZone

--- src/services/date.ts
+++ src/services/date.ts
@@ -1,12 +1,9 @@
-import { 
-  startOfDay, 
-  getUnixTime, 
-  secondsToHours 
-} from 'date-fns'
+import moment from 'moment'
 
 export const getDates = () => {
-  const firstDay = getUnixTime(startOfDay(new Date(`${process.env.NEXT_PUBLIC_DATE}`)))
-  const currentDay = getUnixTime(new Date())
-  const days = Math.floor(secondsToHours(currentDay - firstDay)/24)
-  return days > 0 ? days -1 : days 
+  const tz = new Date().getTimezoneOffset()
+  const start = moment(new Date(`${process.env.NEXT_PUBLIC_DATE}`)).utc()
+  const end = moment().utc().subtract(tz, 'minutes')
+
+  return end.diff(start, 'days')
 }
